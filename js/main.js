b779a0e6fc8056ba52e10b3f53e2060e228cdd13
ezm-k <M46sque96rd@outlook.jp>
2022-10-30 17:47:01 +0900

    ハンバーガーメニューの処理を共通化

--- js/main.js
+++ js/main.js
@@ -1,42 +1,53 @@
 'use strict';
 
 $(function() {
-/*===============================================
-ハンバーガ―メニュー ボタンを押したらクラスを追加/削除
-===============================================*/
+  /*===============================================
+  ハンバーガ―メニュー 共通処理を呼び出し
+  ===============================================*/
   $(".hamburger_menu").click(function() {
-    $(this).toggleClass('active');  //ボタンが×になる
-    $(".hamburger_menu_navi").toggleClass('active');  //ナビが出てくる
+    hamburger();
   });
 
   $(".hamburger_menu_navi a").click(function() {
-    $(".hamburger_menu").removeClass('active');
-    $(".hamburger_menu_navi").removeClass('active');
+    hamburger();
   });
-
-/*=================================================
-Reason  要素が表示されたらクラスを追加
-===================================================*/
+  
+  /*=================================================
+  Reason  要素が表示されたらクラスを追加
+  ===================================================*/
   //スライド左
   $('.inview_slide_left').on('inview', function(event, isInView) {
     if(isInView){
       $(this).stop().addClass('left_appear');
     }
   });
-
+  
   //スライド右
   $('.inview_slide_right').on('inview', function(event, isInView) {
     if(isInView){
       $(this).stop().addClass('right_appear');
     }
   });
-
-/*=================================================
-Voice  要素が表示されたらクラスを追加
-===================================================*/
+  
+  /*=================================================
+  Voice  要素が表示されたらクラスを追加
+  ===================================================*/
   $('.item').on('inview', function(event, isInView) {
     if(isInView){
       $(this).stop().addClass('voice_appear');
     }
   });
 });
+
+/*=================================================
+ハンバーガーメニューの共通処理　
+===================================================*/
+function hamburger() {
+  $('.hamburger_menu').toggleClass('active');
+
+  if ($('.hamburger_menu').hasClass('active')) {
+    $(".hamburger_menu_navi").addClass('active');
+  } else {
+    $(".hamburger_menu_navi").removeClass('active');
+  }
+}
